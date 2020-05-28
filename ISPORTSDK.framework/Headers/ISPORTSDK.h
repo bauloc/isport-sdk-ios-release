26ce7fa0a1a9e6ff61fef1e6c3e1c1dcdd6fdf21
Dang Nam <dainam8x@gmail.com>
2020-05-28 12:03:15 +0700

    Version 1.0.0

---
+++ ISPORTSDK.framework/Headers/ISPORTSDK.h
@@ -0,0 +1,19 @@
+//
+//  ISPORTSDK.h
+//  ISPORTSDK
+//
+//  Created by mcg-corp on 5/28/20.
+//  Copyright © 2020 isport. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ISPORTSDK.
+FOUNDATION_EXPORT double ISPORTSDKVersionNumber;
+
+//! Project version string for ISPORTSDK.
+FOUNDATION_EXPORT const unsigned char ISPORTSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISPORTSDK/PublicHeader.h>
+
+
